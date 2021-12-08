7c7da379e56b67eac6f609c7985ca284c94ace02
Mgh <mgh.5225@gmail.com>
2021-12-08 20:15:52 +0330

    fix writer add multiple readers

--- main.py
+++ main.py
@@ -3,63 +3,81 @@ import reader
 import writer
 import argparse
 import concurrent.futures
+from threading import Lock
+import glob
 
 
 def usage():
-    guide = 'python3 main.py -i <file_name> -o <file_name> -a <alpha> -b <beta> -mf <max_frames> -mw <max_workers>'
+    guide = 'python3 main.py -i <file_name> -o <file_name> -a <alpha> -b <beta> -mf <max_frames> -mw <max_workers> -mr <max_readers>'
     return guide
 
 
 def arguments():
     parse = argparse.ArgumentParser(usage=usage())
     parse.add_argument(
-        '-i', dest='file', help='Input File Name e.g video.mp4', type=str, required=True)
+        '-i', dest='file', help='Input File Name e.g. video.mp4 or Directory e.g. ~/videos/*.mp4', type=str, required=True)
     parse.add_argument(
-        '-o', dest='pdf', help='Output File Name e.g slides.pdf', type=str, required=True)
+        '-o', dest='pdf', help='Output File Name e.g. slides.pdf', type=str, required=True)
     parse.add_argument(
-        '-a', dest='alpha', help='Alpha e.g 5', type=float, default=5)
+        '-a', dest='alpha', help='Alpha e.g. 5', type=float, default=5)
     parse.add_argument(
-        '-b', dest='beta', help='Beta e.g 0', type=float, default=0)
+        '-b', dest='beta', help='Beta e.g. 0', type=float, default=0)
     parse.add_argument(
-        '-mf', dest='max_frames', help='Max Frames e.g 600', type=int, default=600)
+        '-mf', dest='max_frames', help='Max Frames e.g. 600', type=int, default=600)
     parse.add_argument(
-        '-mw', dest='max_workers', help='Max Workers e.g 5', type=int, default=5)
+        '-mw', dest='max_workers', help='Max Workers e.g. 5', type=int, default=3)
+    parse.add_argument(
+        '-mr', dest='max_readers', help='Max Readers e.g. 5', type=int, default=3)
     return parse.parse_args()
 
 
 if __name__ == "__main__":
     args = arguments()
-    fileName = args.file
+    fileNames = glob.glob(args.file)
     pdfName = args.pdf
     alpha = args.alpha
     beta = args.beta
     max_frames = args.max_frames
-    max_workers =args.max_workers
+    max_workers = args.max_workers
+    max_readers = args.max_readers
 
-    rdr = reader.Reader(fileName)
     images = []
-    frames = []
-    writers = []
     writers_frames = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        for (ret, frame) in rdr.read():
-            if ret == True:
-                frames.append(frame)
-                if len(frames) == max_frames:
-                    new_writer = writer.Writer(frames, alpha)
-                    writers.append(executor.submit(new_writer.run))
-                    print("Writer " + str(writer.Writer.writers_number) + " Added")
-                    if beta > 0:
-                        frames = frames[-beta:]
+
+    frame_lock = Lock()
+
+    for fileName in fileNames:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as reader_executor:
+            new_reader = reader.Reader(fileName)
+            frames = []
+            writers = []
+            read_future = reader_executor.submit(new_reader.read)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as writer_executor:
+                for (ret, frame) in read_future.result():
+                    if ret == True:
+                        frames.append(frame)
+                        if len(frames) == max_frames:
+                            new_writer = writer.Writer(frames, alpha)
+                            writers.append(
+                                writer_executor.submit(new_writer.run))
+                            print("Writer " +
+                                  str(writer.Writer.writers_number) + " Added")
+                            if beta > 0:
+                                frames = frames[-beta:]
+                            else:
+                                frames = []
+
                     else:
-                        frames = []
+                        break
+                for f_writer in concurrent.futures.as_completed(writers):
+                    frame_lock.acquire()
+                    writers_frames += f_writer.result()
+                    frame_lock.release()
 
-            else:
-                break
-        for f_writer in concurrent.futures.as_completed(writers):
-            writers_frames += f_writer.result()
-
+    writer.Writer.writers_number = 0
     final_writer = writer.Writer(writers_frames, alpha)
-    for frame in final_writer.run():
+
+    for frame in final_writer.run(True):
         images.append(Image.fromarray(frame))
+
     images[0].save(pdfName, save_all=True, append_images=images[1:])
--- writer.py
+++ writer.py
@@ -12,28 +12,57 @@ class Writer:
     def __init__(self, frames, alpha):
         self.frames = frames
         self.alpha = alpha
+        self.unique_frames = []
+        self.last_frame_index = 0
         Writer.writers_number += 1
 
-    def run(self):
+    def recursive_check(self, cur_frame_index):
+        frame = self.frames[cur_frame_index]
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        for last_frame in self.unique_frames:
+            gray_last_frame = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+
+            frame_diff = cv2.absdiff(gray_frame, gray_last_frame)
+
+            if np.mean(frame_diff) < self.alpha:
+                return False
+        return True
+
+    def normal_check(self, cur_frame_index):
+        frame = self.frames[cur_frame_index]
+        last_frame = self.frames[self.last_frame_index]
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_last_frame = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+
+        frame_diff = cv2.absdiff(gray_frame, gray_last_frame)
+
+        if np.mean(frame_diff) >= self.alpha:
+            return True
+        return False
+
+    def run(self, recursive_check=False):
         if Writer.writers_number == 1:
             self.unique_frames.append(self.frames[0])
 
         while self.last_frame_index < len(self.frames):
             flag = False
-            for i in range(self.last_frame_index+1, len(self.frames)):
-                frame = self.frames[i]
-                last_frame = self.frames[self.last_frame_index]
+            for i in range(self.last_frame_index + 1, len(self.frames)):
+                check = False
 
-                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                gray_last_frame = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+                if recursive_check:
+                    check = self.recursive_check(i)
+                else:
+                    check = self.normal_check(i)
 
-                frame_diff = cv2.absdiff(gray_frame, gray_last_frame)
-
-                if np.mean(frame_diff) >= self.alpha:
-                    self.unique_frames.append(frame)
+                if check:
+                    self.unique_frames.append(self.frames[i])
                     self.last_frame_index = i
                     flag = True
                     break
+
             if flag == False:
                 break
+
         return self.unique_frames
